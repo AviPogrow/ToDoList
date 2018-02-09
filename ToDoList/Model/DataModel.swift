358d8d1479e295d57ae9a2f9df7056baed67438f
Avi Pogrow <rpogrow@gmail.com>
2018-02-09 00:20:43 -0500

    Class DataModel
    Create Swift class to hold all the model objects

---
+++ ToDoList/Model/DataModel.swift
@@ -0,0 +1,12 @@
+//
+//  DataModel.swift
+//  ToDoList
+//
+//  Created by Macbook Pro on 2/9/18.
+//  Copyright © 2018 Macbook Pro. All rights reserved.
+//
+
+import Foundation
+class DataModel {
+    //wrapper class to hold all model objects
+}
